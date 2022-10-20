0c3d48e62f9fbe4f02f0b2cbb0cfb1388957a313
Jordan Hayashi <jhhayashi@gmail.com>
2022-10-20 15:20:25 -0400

    Add repro case calling console.error()

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.error('Calling console.error will crash the app in Expo Go in production mode')
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ README.md
@@ -0,0 +1,16 @@
+# Repro Case for Expo Go Crash
+
+It seems that calling console.error() from the JS when running in Expo Go will
+crash the app.
+
+## Instructions
+
+```
+# install deps
+npm i
+
+# run app
+npm start
+
+# open in expo go and it will crash
+```
